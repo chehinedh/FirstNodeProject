a3e36570c4711ecb9ad6af879ae82202cec7e232
chehinedh <chehine.dhemaied@esprit.tn>
2023-02-17 22:36:55 +0100

    ⬆️ MongoDB

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
@@ -7,6 +9,11 @@ const app = express();
 // listen for requests
 app.listen(3000);
 
+// connect to mongodb
+const db ="mongodb+srv://chehine:chehine123@cluster0.ct5uiny.mongodb.net/node?retryWrites=true&w=majority";
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3001))
+  .catch((err) => console.log(err));
 //middleware & static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
@@ -23,30 +30,51 @@ app.use((res, req, next) => {
 app.set("view engine", "ejs");
 // app.set('views', 'myviews');
 
-app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-  ];
-  res.render("index", { title: "Home", blogs });
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog',
+    snippet: 'about my new blog',
+    body: 'more about my new blog'
+  });
+
+  blog.save()
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) =>{
+      console.log(err);
+    } )
 });
 
-app.get("/about", (req, res) => {
-  res.render("about", { title: "About" });
+app.get('/single-blog', (res, req) => {
+  Blog.findById()
+})
+
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then((result) =>{
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 });
 
+// routes
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new blog" });
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { blogs: result, title: "All blogs" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 // 404 page
